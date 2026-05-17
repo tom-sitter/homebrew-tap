393059098757db27c2bb05dbffccce2ee9ba2081
Tom Sitter <thomassiter@colabsoftware.com>
2026-05-17 15:53:21 -0230

    Add TypeKart formula

---
+++ Formula/typekart.rb
@@ -0,0 +1,24 @@
+class Typekart < Formula
+  desc "Terminal typing racer with kart-style item effects"
+  homepage "https://github.com/tom-sitter/typekart"
+  version "0.1.0"
+  license "GPL-3.0-or-later"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/tom-sitter/typekart/releases/download/v#{version}/typekart-aarch64-apple-darwin.tar.gz"
+      sha256 "c1f88ae09f5ec5fa4c99f0f42378a6f8ff06caee91ca95305ea8d66954f821e1"
+    else
+      url "https://github.com/tom-sitter/typekart/releases/download/v#{version}/typekart-x86_64-apple-darwin.tar.gz"
+      sha256 "669538dba05913989d837fc965925970cdad4ad9831f2d015045704d5ddb4b5c"
+    end
+  end
+
+  def install
+    bin.install "typekart"
+  end
+
+  test do
+    assert_match "A terminal typing racer", shell_output("#{bin}/typekart --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# TypeKart Homebrew Tap
+
+Install TypeKart with:
+
+```sh
+brew install tom-sitter/tap/typekart
+```
+
+Or tap first:
+
+```sh
+brew tap tom-sitter/tap
+brew install typekart
+```
