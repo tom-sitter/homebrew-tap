b2765fb246dec37fdcca6c24d1d81f36ff71cf66
Tom Sitter <thomassiter@colabsoftware.com>
2026-05-17 15:55:31 -0230

    Fix TypeKart formula audit

--- Formula/typekart.rb
+++ Formula/typekart.rb
@@ -1,15 +1,14 @@
 class Typekart < Formula
   desc "Terminal typing racer with kart-style item effects"
   homepage "https://github.com/tom-sitter/typekart"
-  version "0.1.0"
   license "GPL-3.0-or-later"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tom-sitter/typekart/releases/download/v#{version}/typekart-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.0/typekart-aarch64-apple-darwin.tar.gz"
       sha256 "c1f88ae09f5ec5fa4c99f0f42378a6f8ff06caee91ca95305ea8d66954f821e1"
     else
-      url "https://github.com/tom-sitter/typekart/releases/download/v#{version}/typekart-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.0/typekart-x86_64-apple-darwin.tar.gz"
       sha256 "669538dba05913989d837fc965925970cdad4ad9831f2d015045704d5ddb4b5c"
     end
   end
