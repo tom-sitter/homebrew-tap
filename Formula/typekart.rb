8c86ba30cb37202d8f59b1d309ada619c254f530
Tom Sitter <thomassiter@colabsoftware.com>
2026-05-19 15:44:23 -0230

    Update TypeKart to v0.1.5

--- Formula/typekart.rb
+++ Formula/typekart.rb
@@ -5,11 +5,11 @@ class Typekart < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.4/typekart-aarch64-apple-darwin.tar.gz"
-      sha256 "89e7787cbb85fad4bab94dd1181a7af4d97375ce7ffb08ced24460e46e5b8688"
+      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.5/typekart-aarch64-apple-darwin.tar.gz"
+      sha256 "0d1ce06095a3a8c49ed95e9a2b96cb856a0329a49ba93dc39ce2e40be1f6b332"
     else
-      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.4/typekart-x86_64-apple-darwin.tar.gz"
-      sha256 "e81d5b2d8b698061aa916510a707f322dbdf8883c0d926a8dbe1d23d3d6fe3a5"
+      url "https://github.com/tom-sitter/typekart/releases/download/v0.1.5/typekart-x86_64-apple-darwin.tar.gz"
+      sha256 "080c84b4ad222f6c478d5a9dfebfe963216accccb97e4585d38903d1840613c7"
     end
   end
 
